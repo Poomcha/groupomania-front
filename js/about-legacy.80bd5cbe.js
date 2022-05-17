d129aa3945dd1ee4849c896fb1c2e984eb9bfbd0
Ben Degeneve <poompoomcha@gmail.com>
2022-05-17 18:54:03 +0200

    deploy

---
+++ js/about-legacy.80bd5cbe.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkfront"]=self["webpackChunkfront"]||[]).push([[443],{5834:function(n,r,t){t.r(r),t.d(r,{default:function(){return l}});var u=t(6252),a={class:"about"},e=(0,u._)("h1",null,"This is an about page",-1),s=[e];function c(n,r){return(0,u.wg)(),(0,u.iD)("div",a,s)}var f=t(3744);const i={},o=(0,f.Z)(i,[["render",c]]);var l=o}}]);
+//# sourceMappingURL=about-legacy.80bd5cbe.js.map
